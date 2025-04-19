89b52c127368aa8cd9bfc2b6e33b8486dcf61be0
Nima8FT <nima_8ak@yahoo.com>
2025-04-19 16:26:35 +0330

    I uninstalled Fortify because there was a problem with one of the packages and I had to uninstall it and reinstall it.

--- Web-Application/Fortify/resources/views/livewire/logout.blade.php
+++
@@ -1,6 +0,0 @@
-<div>
-    <button wire:click='logout'
-        class="px-4 py-2 border border-red-700 text-white rounded-md hover:bg-red-700 cursor-pointer transition ease-in-out duration-300 font-bold">
-        Logout
-    </button>
-</div>
--- Web-Application/Fortify/resources/views/livewire/profile.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="bg-[#0d1b2a] h-screen flex justify-center items-center flex-col space-y-8">
-
-    <livewire:logout />
-    <livewire:delete-account />
-
-</div>
